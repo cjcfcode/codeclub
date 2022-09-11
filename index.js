bb7be55ebe5a810af92695fb7baaac2f1ffcae06
chengjie <chengjie@cloudflare.com>
2022-09-11 16:42:07 -0500

    assginment 1

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 export default {
-	fetch() {
-		return new Response('Hello worker!', {
+	fetch(request) {
+		if (request.method == "POST") {
+	    	return new Response(request.method, {
 			headers: {
 				'content-type': 'text/plain',
-			},
-		});
+				},
+			});
+		}
+		else{
+			return new Response('wrong method! The method is ' + request.method, {
+			headers: {
+				'content-type': 'text/plain',
+				},
+			});
+		}
 	},
-};
+};	
